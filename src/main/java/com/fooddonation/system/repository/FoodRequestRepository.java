98071362ea0bbf50b0bbbdbf2d26c9bee06b384a
piyush singh <piyushsingh7033457@gmail.com>
2026-05-26 12:59:14 +0530

    railway configs

--- src/main/java/com/fooddonation/system/repository/FoodRequestRepository.java
+++ src/main/java/com/fooddonation/system/repository/FoodRequestRepository.java
@@ -8,24 +8,24 @@ import java.util.List;
 
 public interface FoodRequestRepository extends JpaRepository<FoodRequest, Long> {
 
-    // Donor Requests
+
     List<FoodRequest> findByDonorId(Long donorId);
 
-    // Receiver Requests
+
     List<FoodRequest> findByReceiverId(Long receiverId);
 
-    // Status Based Requests
+
     List<FoodRequest> findByStatus(FoodStatus status);
 
-    // Food Specific Requests
+
     List<FoodRequest> findByFoodId(Long foodId);
 
-    // Admin Pending Requests
+
     List<FoodRequest> findByStatusOrderByCreatedAtDesc(FoodStatus status);
 
-    // Receiver + Status
+
     List<FoodRequest> findByReceiverIdAndStatus(Long receiverId, FoodStatus status);
 
-    // Donor + Status
+
     List<FoodRequest> findByDonorIdAndStatus(Long donorId, FoodStatus status);
 }
