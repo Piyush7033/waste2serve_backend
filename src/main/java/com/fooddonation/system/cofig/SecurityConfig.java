c820d324cdb18ecb4dcda8f90a9d11c8e1bad5ad
piyush singh <piyushsingh7033457@gmail.com>
2026-05-26 13:44:55 +0530

    fix origin allow security

--- src/main/java/com/fooddonation/system/cofig/SecurityConfig.java
+++ src/main/java/com/fooddonation/system/cofig/SecurityConfig.java
@@ -50,7 +50,8 @@ public class SecurityConfig {
         config.setAllowedOrigins(
                 List.of(
                         "http://localhost:5173",
-                        "http://localhost:3000"
+                        "http://localhost:3000",
+                        "https://waste2servebackend-production.up.railway.app"
                 )
         );
 
